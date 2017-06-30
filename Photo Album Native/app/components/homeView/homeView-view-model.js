8780e830d095735643d25da49dc8d0e497594503
Demetre Phipps <demetrephipps@gmail.com>
2017-06-30 21:01:27 +0000

    Changes made by Demetre Phipps (demetrephipps@gmail.com).

--- Photo Album Native/app/components/homeView/homeView-view-model.js
+++ Photo Album Native/app/components/homeView/homeView-view-model.js
@@ -4,33 +4,37 @@ var observable = require("data/observable");
 var imageSourceModule = require("image-source");
 var fileSystemModule = require("file-system");
 //needed for the collection where the image objects will be stored
-var ObservableArrayModule = require("data/observable-array");
+var observableArrayModule = require("data/observable-array");
 //gives access to the image encoding enumeration that will be needed when you upload images to the cloud in Lesson 2. 
 var enums = require("ui/enums");
 
 //
-var localImagesArray = new ObservableArrayModule.ObservableArray();
+var localImagesArray = new observableArrayModule.ObservableArray();
 var directory = "/../../res/";
 
 function imageFromSource(imageName) {
     return imageSourceModule.fromFile(fileSystemModule.path.join(__dirname, directory + imageName));
 };
 //images 1-6 is being pushed, image 7 and 8 will be pushed by a button
-var item1 = { itemImage: imageFromSource("01.jpg") };
-var item2 = { itemImage: imageFromSource("02.jpg") };
-var item3 = { itemImage: imageFromSource("03.jpg") };
-var item4 = { itemImage: imageFromSource("04.jpg") };
-var item5 = { itemImage: imageFromSource("05.jpg") };
-var item6 = { itemImage: imageFromSource("06.jpg") };
+var item1 = {itemImage: imageFromSource("01.jpg")}; 
+var item2 = {itemImage: imageFromSource("02.jpg")}; 
+var item3 = {itemImage: imageFromSource("03.jpg")}; 
+var item4 = {itemImage: imageFromSource("04.jpg")}; 
+var item5 = {itemImage: imageFromSource("05.jpg")}; 
+var item6 = {itemImage: imageFromSource("06.jpg")};
 //pushing images 1-6 to array
 localImagesArray.push([item1, item2, item3, item4, item5, item6]);
 //images 7 and 8
-var item7 = { itemImage: imageFromSource("07.jpg") };
-var item8 = { itemImage: imageFromSource("08.jpg") };
+var item7 = {itemImage: imageFromSource("07.jpg")}; 
+var item8 = {itemImage: imageFromSource("08.jpg")};
 
 //creating the view model as an instance of type Observable
 //bind controls properties from the main page to properties in the view model.
 var photoAlbumModel = new observable.Observable();
+
+photoAlbumModel.set("message", "Add new photos");
+
+
 
 
 Object.defineProperty(photoAlbumModel, "photoItems",{
@@ -42,5 +46,13 @@ Object.defineProperty(photoAlbumModel, "photoItems",{
     configurable: true
 })
 
+
+photoAlbumModel.tapAction = function(){
+    //push images 7 and 8
+    localImagesArray.push(item7);
+    localImagesArray.push(item8);
+    //to inform the user about the new images
+    photoAlbumModel.set("message", "Images added. Total images: "+localImagesArray.length);
+};
 exports.photoAlbumModel = photoAlbumModel;
 
