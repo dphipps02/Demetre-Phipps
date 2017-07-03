1f0762fc2826a3ea1b526b9d86012a98ca19a4e5
Demetre Phipps <demetrephipps@gmail.com>
2017-07-03 14:04:35 +0000

    Changes made. Backend was added for new pictures to uploaded.

--- Photo Album Native/app/components/homeView/homeView-view-model.js
+++ Photo Album Native/app/components/homeView/homeView-view-model.js
@@ -12,6 +12,11 @@ var enums = require("ui/enums");
 var localImagesArray = new observableArrayModule.ObservableArray();
 var directory = "/../../res/";
 
+//
+var Everlive = require('../../everlive.all.min');
+
+
+
 function imageFromSource(imageName) {
     return imageSourceModule.fromFile(fileSystemModule.path.join(__dirname, directory + imageName));
 };
@@ -32,11 +37,15 @@ var item8 = {itemImage: imageFromSource("08.jpg")};
 //bind controls properties from the main page to properties in the view model.
 var photoAlbumModel = new observable.Observable();
 
+//this will prompt the user to add more
 photoAlbumModel.set("message", "Add new photos");
 
+//loading module responsible for camera API
+var cameraModule = require("camera");
 
 
 
+/*
 Object.defineProperty(photoAlbumModel, "photoItems",{
 
     get: function(){
@@ -44,15 +53,57 @@ Object.defineProperty(photoAlbumModel, "photoItems",{
     }, 
     enumerable: true, 
     configurable: true
-})
+})*/
+
+var backendArray = new observableArrayModule.ObservableArray();
+
+Object.defineProperty(photoAlbumModel, "photoItems", {
+   //retrieves all pictures stored in your backend. 
+    get: function () {
+        everlive.Files.get().then(function (data) {
+                data.result.forEach(function (fileMetadata) {
+                    imageSourceModule.fromUrl(fileMetadata.Uri).then(function (result) {
+                        var item = {
+                            itemImage: result
+                        };
+                        backendArray.push(item);
+                    });
+                });
+            },
+            function (error) {});
+
+        return backendArray;
+    },
+    enumerable: true,
+    configurable: true
+});
+
+var everlive = new Everlive("3qb4uw998op7embd");
 
 
 photoAlbumModel.tapAction = function(){
-    //push images 7 and 8
-    localImagesArray.push(item7);
-    localImagesArray.push(item8);
-    //to inform the user about the new images
-    photoAlbumModel.set("message", "Images added. Total images: "+localImagesArray.length);
+     cameraModule.takePicture({
+        width: 300,
+        height: 300,
+        keepAspectRatio: true
+    }).then(function (picture) {
+        var item = {
+            itemImage: picture
+        };
+        backendArray.push(item);
+
+        var file = {
+            "Filename": Math.random().toString(36).substring(2, 15) + ".jpg",
+            "ContentType": "image/jpeg",
+            "base64": picture.toBase64String(enums.ImageFormat.jpeg, 100)
+        };
+
+        everlive.Files.create(file,
+            function (data) {},
+            function (error) {});
+    });
 };
+
+
 exports.photoAlbumModel = photoAlbumModel;
 
